5d3c56a8a314066540284c1b97d5a7fe3fdc92f3
omgunicorns <36963220+omgunicorns@users.noreply.github.com>
2018-04-21 04:31:18 +0300

    re-struct

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,5 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "keyhandler.cpp"
 
-#include <QPixmap>
-#include <QMessageBox>
-#include <QKeyEvent>
-#include <QtMultimedia/QSound>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -23,54 +18,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     if (event->type()==QEvent::KeyPress) {
-            QKeyEvent* key = static_cast<QKeyEvent*>(event);
-            //QMessageBox::information(this, "zag", "text");
-
-            switch (key->key()){
-                case Qt::Key_A:
-                    QSound::play("/Users/petr/CProject/sounds/wav/1.wav");
-                    break;
-                case Qt::Key_W:
-                    QSound::play("/Users/petr/CProject/sounds/wav/2.wav");
-                    break;
-                case Qt::Key_S:
-                    QSound::play("/Users/petr/CProject/sounds/wav/3.wav");
-                    break;
-                case Qt::Key_E:
-                    QSound::play("/Users/petr/CProject/sounds/wav/4.wav");
-                    break;
-                case Qt::Key_D:
-                    QSound::play("/Users/petr/CProject/sounds/wav/5.wav");
-                    break;
-                case Qt::Key_F:
-                    QSound::play("/Users/petr/CProject/sounds/wav/6.wav");
-                    break;
-                case Qt::Key_T:
-                    QSound::play("/Users/petr/CProject/sounds/wav/7.wav");
-                    break;
-                case Qt::Key_G:
-                    QSound::play("/Users/petr/CProject/sounds/wav/8.wav");
-                    break;
-                case Qt::Key_Y:
-                    QSound::play("/Users/petr/CProject/sounds/wav/9.wav");
-                    break;
-                case Qt::Key_H:
-                    QSound::play("/Users/petr/CProject/sounds/wav/10.wav");
-                    break;
-                case Qt::Key_U:
-                    QSound::play("/Users/petr/CProject/sounds/wav/11.wav");
-                    break;
-                case Qt::Key_J:
-                    QSound::play("/Users/petr/CProject/sounds/wav/12.wav");
-                    break;
-                case Qt::Key_K:
-                    QSound::play("/Users/petr/CProject/sounds/wav/13.wav");
-                    break;
-
-            }
+        KeyHandler(event);
     }
 
 }
